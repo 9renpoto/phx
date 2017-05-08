749314d1a38ec2cf0546088fa350c81cc5fdbcd8
Keisuke Kan <9renpoto@gmail.com>
2017-05-09 00:28:56 +0900

    fix(mix) fix coverage config

--- mix.exs
+++ mix.exs
@@ -2,12 +2,20 @@ defmodule Ex.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :ex,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :ex,
+      version: "0.1.0",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.html": :test,
+        "coveralls.json": :test,
+      ],
+      test_coverage: [tool: ExCoveralls],
+    ]
   end
 
   def application do
@@ -18,7 +26,7 @@ defmodule Ex.Mixfile do
     [
       {:excoveralls, "~> 0.5.7"},
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
-      {:credo, "~> 0.7", only: [:dev, :test]}
+      {:credo, "~> 0.7", only: [:dev, :test]},
     ]
   end
 end
